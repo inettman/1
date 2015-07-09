932f48e53e67eaf069da48df3ff800b1e78aa09d
Igor N <inettman@gmail.com>
2015-07-09 23:15:26 +0300

    Add sizes

--- src/Location/PlaceBundle/Coordinate.php
+++ src/Location/PlaceBundle/Coordinate.php
@@ -4,11 +4,11 @@ namespace Location\PlaceBundle;
 
 class Coordinate
 {
-    public function getDistance($begin, $end, $unit = false)
+    public function getDistance($begin, $end, $unit = false, $round = 0)
     {   
         $result = round(2 * asin(sqrt( pow(sin(deg2rad( ($begin['lat']-$end['lat']) / 2)), 2) + cos(deg2rad($begin['lat'])) * cos(deg2rad($end['lat'])) * pow(sin(deg2rad(($begin['lng']- $end['lng']) / 2)), 2))) * 6378245);
         if($unit == 'km'){
-           $result = round($result/1000, 1);
+           $result = round($result/1000, $round);
         }
         return $result;
     }
