f4f66178c43aadc306518f1cdf2e535ac4093533
Igor N <igor.samua@gmail.com>
2015-10-10 17:16:13 +0300

    Added weather

--- src/Location/PlaceBundle/Controller/CityController.php
+++ src/Location/PlaceBundle/Controller/CityController.php
@@ -59,5 +59,63 @@ class CityController extends Controller
         
         return $this->render('LocationPlaceBundle:City:map.html.twig', array('map' => $map, 'title'=>$title, 'description'=>$description, 'city'=>$city, 'places'=>$places, 'cities_nearest'=>$cities_nearest));
     }
+    
+    public function weatherAction($id)
+    {
+        $city = $this->getDoctrine()
+            ->getRepository('LocationPlaceBundle:City')
+            ->find($id);
+
+        if (!$city) {
+            throw $this->createNotFoundException(
+                'No city found'
+            );
+        }
+
+        $cities_nearest = $this->getDoctrine()
+            ->getRepository('LocationPlaceBundle:City')
+            ->getNearestCities($city->getLat(), $city->getLng());
+        
+        $translater = $this->get('translator');
+        
+        $district = $city->getDistrict();
+        
+        $region = $district->getRegion();
+        
+        $country = $region->getCountry();
+        
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addRouteItem($translater->trans('page_main'), 'location_page_index');
+        $breadcrumbs->addRouteItem($country->getName(), 'location_country_map', array('id'=>$country->getId()));
+        $breadcrumbs->addRouteItem($region->getName(), 'location_region_map', array('id'=>$region->getId()));
+        $breadcrumbs->addRouteItem($district->getName(), 'location_district_map', array('id'=>$district->getId()));
+        $breadcrumbs->addRouteItem($city->getName(), 'location_city_weather', array('id'=>$city->getId()));
+        
+        $title = $city->getName().' '.$translater->trans('weather');
+        
+        $district_name = $city->getGooglePlaceId()!=$district->getGooglePlaceId()?$district->getName().', ':'';
+        
+        $description = $city->getName().' '.$translater->trans('weather forecast').', '.$translater->trans('hourly weather').', '.$district_name.$region->getName().', '.$country->getName();
+        
+        $coordinate = $this->get('coordinate');
+        
+        $weather = $coordinate->getWeather($city->getLat(), $city->getLng(),  $request = $this->get('request')->getLocale());
+        
+        $wind_directions = explode(',',$translater->trans('wind directions'));
+        
+        $map = $this->get('ivory_google_map.map');
+        
+        $map->setAutoZoom(true);
+        
+        $map->setBound($city->getLatS(), $city->getLngW(), $city->getLatN(), $city->getLngE(), true, true);
+        
+        $map_var = $map->getJavascriptVariable();
+        
+        $vars = array();
+        $vars['map'] = $map_var;
+        
+        return $this->render('LocationPlaceBundle:City:weather.html.twig', array('map' => $map, 'vars' => $vars, 'title'=>$title, 'description'=>$description, 'city'=>$city, 'weather' => $weather, 'wind_directions' => $wind_directions, 'cities_nearest'=>$cities_nearest));
+        
+    }
 
 }
--- src/Location/PlaceBundle/Coordinate.php
+++ src/Location/PlaceBundle/Coordinate.php
@@ -12,5 +12,49 @@ class Coordinate
         }
         return $result;
     }
+    
+    public function getWeather($lat, $lng, $lang)
+    {   
+        $result = array();
+        $api_key = 'eef557a0a4fbb5cc7602fd24d2e8f5f9';
+        //$wind_short_names = array("N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW");
+        
+        $current = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$lng.'&units=metric&lang='.$lang.'&APPID='.$api_key), TRUE);
+        
+        if (empty($current['main']['temp'])){
+            $result['current']['temp'] = '...';
+        } else {
+            $result['current']['temp'] = round($current['main']['temp']);
+        }
+ 
+        $result['current']['description'] = $current['weather'][0]['description'];
+        $result['current']['icon'] = 'http://openweathermap.org/img/w/'.$current['weather'][0]['icon'].'.png';
+        $result['current']['date'] = $current['dt'];
+        $result['current']['pressure'] = $current['main']['pressure'];
+        $result['current']['humidity'] = $current['main']['humidity'];
+        $result['current']['wind_deg'] = $current['wind']['deg'];
+        $result['current']['wind_speed'] = $current['wind']['speed'];
+        $result['current']['wind_side'] = round(($current['wind']['deg'] -11.25) / 22.5);
+
+        $hourly = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/forecast?lat='.$lat.'&lon='.$lng.'&units=metric&lang='.$lang.'&APPID='.$api_key.'&cnt=8'), TRUE);
+
+        foreach ($hourly['list'] as $key => $item) {
+            $result['hourly'][$key]['temp'] = round($item['main']['temp']);
+            $result['hourly'][$key]['description'] = $item['weather'][0]['description'];
+            $result['hourly'][$key]['icon'] = 'http://openweathermap.org/img/w/'.$item['weather'][0]['icon'].'.png';
+            $result['hourly'][$key]['date'] = $item['dt'];
+            $result['hourly'][$key]['pressure'] = $item['main']['pressure'];
+            $result['hourly'][$key]['humidity'] = $item['main']['humidity'];
+            $result['hourly'][$key]['wind_deg'] = $item['wind']['deg'];
+            $result['hourly'][$key]['wind_speed'] = $item['wind']['speed'];
+            $result['hourly'][$key]['wind_side'] = round(($item['wind']['deg'] -11.25) / 22.5);
+        }
+
+        //$result['daily'] = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=16&mode=json&lat='.$lat.'&lon='.$lng.'&units=metric&lang='.$lang.'&APPID='.$api_key), TRUE);
+        
+        $result['key'] = $api_key;
+        
+        return $result;
+    }
 
 }
